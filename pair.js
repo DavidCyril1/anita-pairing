c0527ea740843ca90350f24cbbf42a5e205dfcdc
DavidCyril1 <olah4x8@gmail.com>
2025-04-30 09:56:53 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,9 +1,10 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
-const {makeid} = require('./id');
+const PastebinAPI = require('pastebin-js');
+const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router();
+const path = require('path');
+const router = express.Router();
 const pino = require("pino");
 const {
     default: Maher_Zubair,
@@ -25,48 +26,35 @@ router.get('/', async (req, res) => {
     async function SIGMA_MD_PAIR_CODE() {
         const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
         try {
-            let Pair_Code_By_Maher_Zubair = Maher_Zubair({
+            const Pair_Code_By_Maher_Zubair = Maher_Zubair({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
                 },
                 printQRInTerminal: false,
                 logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: ["Chrome (Linux)", "", ""]
+                browser: Browsers.macOS("Desktop")
             });
 
             if (!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
                 const code = await Pair_Code_By_Maher_Zubair.requestPairingCode(num);
+
                 if (!res.headersSent) {
-                    await res.send({ code });
+                    res.send({ code });
                 }
             }
 
-        
-           Pair_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds);
+            Pair_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds);
 
-Pair_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
-    const { connection, lastDisconnect } = s;
-    if (connection == "open") {
-        await delay(5000);
+            Pair_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection === "open") {
+                    await delay(5000);
 
-
-           
-        // Read the contents of the creds.json file as text
-        const data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`, 'utf-8');
-        await delay(800);
-
-        // Send the content as a text message
-        await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, {
-            text: `Here is the content of creds.json:\n\n${data}`
-        });
-    }
-});
-        }
-
-         let SIGMA_MD_TEXT = `
+                    // Send the welcome/info text
+                    const SIGMA_MD_TEXT = `
   ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 ❶  || *ᴡʜᴀᴛsᴀᴘᴘ ᴄʜᴀɴɴᴇʟ* = https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
@@ -75,29 +63,44 @@ Pair_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
 ➌ || *ʏᴏᴜᴛᴜʙᴇ* = https://www.youtube.com/@DavidCyril_TECH 
 ▬▬▬▬▬▬▬▬▬▬▬▬
 THIS IS YOUR SESSION ID👇`;
-                    
-                    await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: SIGMA_MD_TEXT });
 
+                    await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, {
+                        text: SIGMA_MD_TEXT
+                    });
 
+                    await delay(800);
 
+                    // Read and send creds.json content as text
+                    const credsPath = path.join(__dirname, `/temp/${id}/creds.json`);
+                    const data = fs.readFileSync(credsPath, 'utf-8');
+
+                    await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, {
+                        text: `\`\`\`json\n${data}\n\`\`\``
+                    });
 
                     await delay(100);
                     await Pair_Code_By_Maher_Zubair.ws.close();
-                    return await removeFile('./temp/' + id);
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    removeFile('./temp/' + id);
+                } else if (
+                    connection === "close" &&
+                    lastDisconnect &&
+                    lastDisconnect.error &&
+                    lastDisconnect.error.output.statusCode !== 401
+                ) {
                     await delay(10000);
                     SIGMA_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
-            console.log("service restarted");
-            await removeFile('./temp/' + id);
+            console.log("service restarted", err);
+            removeFile('./temp/' + id);
             if (!res.headersSent) {
-                await res.send({ code: "Service Unavailable" });
+                res.send({ code: "Service Unavailable" });
             }
         }
     }
-    return await SIGMA_MD_PAIR_CODE();
+
+    await SIGMA_MD_PAIR_CODE();
 });
 
 module.exports = router;
