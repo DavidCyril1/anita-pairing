23806f279ddb8e7c0406d569bf5513f6304dd8dc
DavidCyril1 <olah4x8@gmail.com>
2025-05-28 17:55:49 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -2,7 +2,7 @@ const PastebinAPI = require('pastebin-js'),
 pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
 
 const { makeid } = require('./id');
-const createToxxicStore = require('./toxxic-store'); // Added
+const createToxxicStore = require('./store'); // Added
 const express = require('express');
 const fs = require('fs');
 let router = express.Router();
