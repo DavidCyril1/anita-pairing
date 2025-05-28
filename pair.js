f3bc9ea49ffc7744cb2b7fed1f2d1fdf8f177d45
DavidCyril1 <olah4x8@gmail.com>
2025-05-28 18:30:42 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router();
 const pino = require("pino");
 
-if (!fs.existsSync('./store)) {
+if (!fs.existsSync('./store')) {
     fs.mkdirSync('./store');
 }
 
