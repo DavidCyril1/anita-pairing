be4cb61e4f80176e0175d18c622717f093e063e0
DavidCyril1 <olah4x8@gmail.com>
2025-09-09 11:47:21 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -10,7 +10,7 @@ const {
     delay,
     fetchLatestBaileysVersion,
     DisconnectReason
-} = require("baileys-mod");
+} = require("baileys");
 const { Boom } = require('@hapi/boom');
 
 function removeFile(FilePath) {
@@ -106,8 +106,8 @@ router.get('/', async (req, res) => {
             if (!sock.authState.creds.registered) {
                 setTimeout(async () => {
                     try {
-                        const customPairCode = "CYRILDEV"; // Custom 8-character pairing code
-                        const code = await sock.requestPairingCode(num, customPairCode);
+                       
+                        const code = await sock.requestPairingCode(num);
                         console.log('Pairing code generated:', code, 'for:', num);
                         
                         if (!res.headersSent) {
